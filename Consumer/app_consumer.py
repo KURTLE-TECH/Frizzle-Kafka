140982f29ac198f76166976f38a18029cb4f9465
Rishith Bhowmick <brishi00@gmail.com>
2021-03-29 11:29:07 +0530

    Corrected output of exception

--- Consumer/app_consumer.py
+++ Consumer/app_consumer.py
@@ -10,11 +10,8 @@ db_handler = db.DynamodbHandler("pes_node_2")
 try:
         for message in consumer:            
             print(loads(message))
-        # node_sensor_values = request.get_json()
-        # producer.send('pes_node_2', value=node_sensor_values)
-        # print(node_sensor_values)
-        node_sensor_values = loads(message)
-        response_body = db_handler.insert(node_sensor_values)
+            node_sensor_values = loads(message)
+            response_body = db_handler.insert(node_sensor_values)
 
 except Exception as e:            
-            return {"Status":"Failed","reason":e}
+        print({"Status":"Failed","reason":e})
