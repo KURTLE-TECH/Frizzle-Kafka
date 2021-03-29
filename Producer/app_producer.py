f86dc0cf7cebc61e9c4d66ff9b97296203c4b2e9
Rishith Bhowmick <brishi00@gmail.com>
2021-03-29 11:54:10 +0530

    Added dumps from json module

--- Producer/app_producer.py
+++ Producer/app_producer.py
@@ -1,30 +1,33 @@
 import boto3
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
 from flask_assets import Bundle, Environment
 from database import DynamodbHandler as db
 from kafka import KafkaProducer
+from json import dumps
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
-                         value_serializer=lambda x: 
+                         value_serializer=lambda x:
                          dumps(x).encode('utf-8'))
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-    if request.method=="GET":
+    if request.method == "GET":
         return 'Hello, World!'
     else:
         return "Root method uses only GET, Please try again"
-    
+
+
 @app.route("/push_to_queue", methods=['POST'])
 def push_to_queue():
-    if request.method== "POST":
+    if request.method == "POST":
         try:
-        node_sensor_values = request.get_json()
-        producer.send('pes_node_2', value=node_sensor_values)
-        # print(node_sensor_values)
-        # response_body = db_handler.insert(node_sensor_values)
-            return {"Status":"Successful"}
-        except Exception as e:            
-            return {"Status":"Failed","reason":e}
+            node_sensor_values = request.get_json()
+            producer.send('pes_node_2', value=node_sensor_values)
+            # print(node_sensor_values)
+            # response_body = db_handler.insert(node_sensor_values)
+            return {"Status": "Successful"}
+        except Exception as e:
+            return {"Status": "Failed", "reason": e}
     else:
         return "POST method only"
