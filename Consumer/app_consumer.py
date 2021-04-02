58d21c1348c3b7032ab42a64e3c1a5d761af0cb7
Ubuntu <ubuntu@ip-10-0-1-215.ap-south-1.compute.internal>
2021-04-02 06:36:24 +0000

    Changed topic in consumer

--- Consumer/app_consumer.py
+++ Consumer/app_consumer.py
@@ -5,13 +5,13 @@ from database import DynamodbHandler as db
 from kafka import KafkaConsumer
 from json import loads
 
-consumer = KafkaConsumer('pes_node_2')
-db_handler = db.DynamodbHandler("pes_node_2")        
-try:
-        for message in consumer:            
-            print(loads(message))
-            node_sensor_values = loads(message)
-            response_body = db_handler.insert(node_sensor_values)
-
-except Exception as e:            
-        print({"Status":"Failed","reason":e})
+consumer = KafkaConsumer('node-3',bootstrap_servers=["13.232.244.184:9092"],
+	auto_offset_reset='latest',
+     	enable_auto_commit=True,
+	value_deserializer=lambda x:loads(x.decode('utf-8'))
+	)
+db_handler = db.DynamodbHandler("pes_node_2")
+for message in consumer:
+	print(message.value)
+#	if not message.value:
+	response_body = db_handler.insert(message.value)
