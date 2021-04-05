d1116497ff34374a41633661f1275539f1c97f56
Rishith Bhowmick <brishi00@gmail.com>
2021-04-05 10:25:32 +0530

    Able to access request data

--- Producer/app.py
+++ Producer/app.py
@@ -1,9 +1,9 @@
 import boto3
 from flask import Flask, render_template, request
 from flask_assets import Bundle, Environment
-
+from datetime import datetime
 from kafka import KafkaProducer
-from json import dumps
+from json import dumps,loads
 #producer = KafkaProducer(bootstrap_servers=['13.232.244.184:9092'],
 #                        value_serializer=lambda x: 
 #                       dumps(x).encode('utf-8'))
@@ -22,9 +22,10 @@ def hello_world():
 def push_to_queue():
 	if request.method == "POST":
 		try:
-			node_sensor_values = request.body.get_json()
+			node_sensor_values = loads(request.data)
 			node_sensor_values['time-stamp'] = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-#			producer.send('node-3', value=node_sensor_values)
+			
+			# producer.send('node-3', value=node_sensor_values)
 	    # print(node_sensor_values)
             # response_body = db_handler.insert(node_sensor_values)
 			return {"Status": "Successful"}
