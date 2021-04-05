73effb4529880e2cfb94b3a04898c1e087e37007
Rishith Bhowmick <brishi00@gmail.com>
2021-04-05 12:23:54 +0530

    corrected formatting

--- Producer/app.py
+++ Producer/app.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template, request
 from flask_assets import Bundle, Environment
 from datetime import datetime
 from kafka import KafkaProducer
-from json import dumps,loads
-#producer = KafkaProducer(bootstrap_servers=['13.232.244.184:9092'],
-#                        value_serializer=lambda x: 
+from json import dumps, loads
+# producer = KafkaProducer(bootstrap_servers=['13.232.244.184:9092'],
+#                        value_serializer=lambda x:
 #                       dumps(x).encode('utf-8'))
 app = Flask(__name__)
 
@@ -20,19 +20,19 @@ def hello_world():
 
 @app.route("/push_to_queue", methods=['POST'])
 def push_to_queue():
-	if request.method == "POST":
-		try:
-			node_sensor_values = loads(request.data)
-			node_sensor_values['time-stamp'] = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-			
-			# producer.send('node-3', value=node_sensor_values)
-	    # print(node_sensor_values)
-            # response_body = db_handler.insert(node_sensor_values)
-			return {"Status": "Successful"}
-		except Exception as e:
-			return {"Status": "Failed", "reason": e}
-	else:
-		return "POST method only"
+    if request.method == "POST":
+        try:
+            node_sensor_values = loads(request.data)
+            node_sensor_values['time-stamp'] = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+            # producer.send('node-3', value=node_sensor_values)
+			# print(node_sensor_values)
+			# response_body = db_handler.insert(node_sensor_values)
+            return {"Status": "Successful","Current":"Just accepting request only"}
+        except Exception as e:
+            return {"Status": "Failed", "reason": e}
+    else:
+        return "POST method only"
+
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
