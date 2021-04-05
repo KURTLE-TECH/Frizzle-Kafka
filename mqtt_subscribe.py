b1bfa73dae32e1b9b77e2cec1364fd8f32856b63
Rishith Bhowmick <brishi00@gmail.com>
2021-04-05 15:43:54 +0530

    Able to parse mqtt data

--- mqtt_subscribe.py
+++ mqtt_subscribe.py
@@ -3,8 +3,8 @@ from sys import getsizeof
 from datetime import datetime
 from json import loads, dumps
 from kafka import KafkaProducer
-#producer = KafkaProducer(bootstrap_servers=[
-#                          '13.232.244.184:9092'], value_serializer=lambda x: dumps(x).encode('utf-8'))
+producer = KafkaProducer(bootstrap_servers=[
+                         '13.232.244.184:9092'], value_serializer=lambda x: dumps(x).encode('utf-8'))
 
 
 def on_connect(client, userdata, flags, rc):
@@ -14,10 +14,12 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
 	# node_sensor_values = loads(msg.payload)
-	node_sensor_values = loads(msg.payload)
-	node_sensor_values['time-stamp'] = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+	# node_sensor_values = loads(msgpayload)
+	#node_sensor_values['time-stamp'] = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+	#print(node_sensor_values)
+	node_sensor_values = msg.payload.decode('utf-8')
 	print(node_sensor_values)
-	#producer.send('node-1', value=node_sensor_values)
+	producer.send('node-1', value=node_sensor_values)
     #print(msg.topic+" "+str(msg.payload))
 
 
@@ -29,4 +31,4 @@ client.on_message = on_message
 
 client.username_pw_set("frizzle_test", "FRIZZLE")
 client.connect(mqttServer, 1883, 60)
-client.loop_forever()
+client.loop_forever()	
