e3b0d72009d031ba2ead424aa5915d2476aa4c96
Rishith Bhowmick <brishi00@gmail.com>
2021-04-10 11:03:09 +0530

    Testing the AdminClient API

--- create_topic.py
+++ create_topic.py
@@ -1,7 +1,15 @@
 from kafka import KafkaAdminClient
 from kafka.admin import NewTopic
-client = KafkaAdminClient(bootstrap_servers=['localhost:9092'])
-topics_list = list()
-topics_list.append(NewTopic(name="node-3", num_partitions=1, replication_factor=1))
-client.create_topics(new_topics = topics_list)
+try:
+    client = KafkaAdminClient(bootstrap_servers=['13.126.242.56:9092'])
+    topics_list = list()
+    topics_list.append(NewTopic(name="node-3", num_partitions=1, replication_factor=1))
+    try:
+        client.create_topics(new_topics = topics_list)
+    except Exception as e:
+        print("Creation error ")
+        print(e)
+except Exception as e:
+    print("Initial config ")
+    print(e)
 
