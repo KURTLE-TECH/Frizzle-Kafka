842b020825d333b62c555d96eea5092e75c36ac8
Rishith Bhowmick <brishi00@gmail.com>
2021-04-12 12:09:54 +0530

    added the table creation as well

--- Producer/app.py
+++ Producer/app.py
@@ -1,4 +1,3 @@
-import boto3
 from flask import Flask, render_template, request
 from flask_assets import Bundle, Environment
 from datetime import datetime
@@ -8,12 +7,15 @@ from kafka import KafkaAdminClient
 from kafka.cluster import ClusterMetadata
 from kafka.admin import NewTopic
 from flask.json import jsonify
+from create_table import *
 import pytz
 import uuid
-# producer = KafkaProducer(bootstrap_servers=['13.232.244.184:9092'],
-#                        value_serializer=lambda x:
-#                       dumps(x).encode('utf-8'))
+producer = KafkaProducer(bootstrap_servers=['13.232.244.184:9092'],
+                       value_serializer=lambda x:
+                      dumps(x).encode('utf-8'))
+
 client = KafkaAdminClient(bootstrap_servers=['13.126.242.56:9092'])
+client = boto3.client('dynamodb')
 app = Flask(__name__)
 
 
@@ -25,28 +27,12 @@ def hello_world():
         return "Root method uses only GET, Please try again"
 
 
-@app.route("/push_to_queue", methods=['POST'])
-def push_to_queue():
-    if request.method == "POST":
-        try:
-            node_sensor_values = loads(request.data)
-            node_sensor_values['time-stamp'] = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-            # producer.send('node-1', value=node_sensor_values)
-			# print(node_sensor_values)
-			# response_body = db_handler.insert(node_sensor_values)
-            return {"Status": "Successful","Current":"Just accepting request only"}
-        except Exception as e:
-            return {"Status": "Failed", "reason": e}
-    else:
-        return "POST method only"
-
 @app.route("/register_node",methods=["POST"])
 def register_node():
     if request.method=="POST":
         try:
             node_info =loads(request.data)
         except Exception as e:
-            # print(e)
             return jsonify({"parsing error ":str(e)})
         print(node_info)
         if node_info["Device ID"] == "":
@@ -57,14 +43,19 @@ def register_node():
             data['date'] = current_time.split()[0]
             data['time'] = current_time.split()[1].rstrip('+5:30')
             topic_name = str(uuid.uuid4())
-            
+            #creating topics for
             topics_list = list()
             topics_list.append(NewTopic(name=topic_name, num_partitions=1, replication_factor=3))
-            
+
             try:
             	client.create_topics(new_topics = topics_list)
             except Exception as e:
             	return jsonify(data)
+
+            status = create_table_in_database(topic_name)
+            if status=="failed":
+                return jsonify(data)
+
             data['id'] = topic_name
             return jsonify(data)
         else:
@@ -76,7 +67,7 @@ def register_node():
             data['date'] = current_time.split()[0]
             data['time'] = current_time.split()[1].rstrip('+5:30')
             return jsonify(data)
-            
+
 
     else:
         return {"Status":"Failed","Reason":"Wrong method, POST method only"}
@@ -84,8 +75,6 @@ def register_node():
 @app.route("/show_topics",methods=["GET"])
 def show_topics():
     try:
-        # connection = ClusterMetadata(bootstrap_servers=['13.126.242.56:9092'])
-        # print(connection.topics())
         return "working"
     except Exception as e:
         return dumps(str(e))
@@ -93,4 +82,3 @@ def show_topics():
 
 if __name__ == "__main__":
     app.run(debug=True,port=5000)
-
--- create_topic.py
+++ create_topic.py
@@ -5,7 +5,8 @@ try:
     topics_list = list()
     topics_list.append(NewTopic(name="node-3", num_partitions=1, replication_factor=1))
     try:
-        client.create_topics(new_topics = topics_list)
+        # client.create_topics(new_topics = topics_list)
+        # client.create_topics(new_topics = topics_list)
     except Exception as e:
         print("Creation error ")
         print(e)
