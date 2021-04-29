8f7753d230834a76c050dc946aa3ae21f1749f4d
Ubuntu <ubuntu@ip-10-0-1-215.ap-south-1.compute.internal>
2021-04-29 23:36:27 +0530

    added fixes

--- mqtt_receiver.py
+++ mqtt_receiver.py
@@ -3,24 +3,29 @@ from sys import getsizeof
 from datetime import datetime
 from json import loads, dumps
 from kafka import KafkaProducer
-producer = KafkaProducer(bootstrap_servers=[
-                          '13.126.242.56:9092'], value_serializer=lambda x: dumps(x).encode('utf-8'))
+#producer = KafkaProducer(bootstrap_servers=[
+#                          ''], value_serializer=lambda x: dumps(x).encode('utf-8'))
 
+from confluent_kafka import Producer
+import socket
 
+conf = {'bootstrap.servers': "13.126.242.56:9092",'client.id':'13.126.242.56'}
+
+producer = Producer(conf)
+producer.flush()
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code" + str(rc))
-
     client.subscribe("Frizzle/Sensor_Data")
 
 
 def on_message(client, userdata, msg):
 	node_sensor_values = eval(msg.payload.decode('utf-8'))
-	print(node_sensor_values)
+	print(node_sensor_values['Device ID'])
 	if 'time-stamp' not in node_sensor_values.keys():
 		node_sensor_values['time-stamp'] = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-	print(node_sensor_values['Device ID'])
-	producer.send(node_sensor_values['Device ID'], value=node_sensor_values)
-
+	#print(node_sensor_values['Device ID'])
+	producer.produce(node_sensor_values['Device ID'],key=node_sensor_values['Device ID'], value=dumps(node_sensor_values))
+	print("sent to ",node_sensor_values['Device ID'])
 
 mqttServer = "13.126.242.56"
 
