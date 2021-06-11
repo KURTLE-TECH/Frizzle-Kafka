d2b36976ce329d23aa3e8c0b0c50530511944cd3
Rishith Bhowmick <brishi00@gmail.com>
2021-06-11 15:59:49 +0530

    added new routes for node registration workflow

--- node_registration/app.py
+++ node_registration/app.py
@@ -1,82 +1,153 @@
+import json
 import boto3
 from flask import Flask, render_template, request
 from flask_assets import Bundle, Environment
-from datetime import datetime
+from datetime import date, datetime
 from json import dumps, loads
-from kafka import KafkaProducer
-from kafka.cluster import ClusterMetadata
-from kafka.admin import NewTopic
 from flask.json import jsonify
 from database import DynamodbHandler as db
 import pytz
 import uuid
-producer = KafkaProducer(bootstrap_servers=['13.126.242.56:9092'],
-                         value_serializer=lambda x:
-                         dumps(x).encode('utf-8'))
+import logging
 
 db_handler = db.DynamodbHandler()
 nodes_table = db_handler.db.Table("Nodes_Available")
 app = Flask(__name__)
 
+# logging.basicConfig(filename='server_nodes.log', filemode="w", level=logging.DEBUG)
+# formatter = logging.Formatter("Level:%(levelname)s %(name)s : %(message)s")
+# handler = logging.FileHandler("requests_nodes.log", mode="w")
+# handler.setFormatter(formatter)
+# app_logger = logging.getLogger("requests")
+# app_logger.setLevel(logging.INFO)
+# app_logger.addHandler(handler)
 
-@app.route('/')
-def hello_world():
-    if request.method == "GET":
-        return 'Hello, World!'
-    else:
-        return "Root method uses only GET, Please try again"
+
+@app.route('/status/server/nodes', methods=["GET"])
+def status():
+    return {"Server": "Node creation and registration", "Status": "Up"}
 
 
-@app.route("/register_node", methods=["POST"])
+@app.route("/create", methods=["GET"])
 def register_node():
-    if request.method == "POST":
+    data = dict()
+    data["Device ID"] = str(uuid.uuid4())
+    date = datetime.now()
+    tz = pytz.timezone('Asia/Kolkata')
+    current_time = str(date.astimezone(tz))      
+    row = {"Device ID": {"S": data["Device ID"]},
+           "registration": {"S": "incomplete"},
+           "generation-time":{"S":current_time}}
+    try:
+        db_handler.client.put_item(
+            TableName="Nodes_Available",
+            Item=row
+        )
+    except Exception as e:
+        print(e)
+        return {"status": "failed", "reason": str(e)}
+    return jsonify(data)
+
+
+@app.route("/register",methods=["POST"])
+def register():
+    try:
+        node_info = loads(request.data)
+    except Exception as e:
+        # add logs here
+        return jsonify({"parsing error ": str(e)})
+    # print(node_info)
+    if node_info["Device ID"] == "":
+        return {'status': "failed", "reason": 'empty device id'}
+
+    response = db_handler.client.get_item(
+        TableName='Nodes_Available',
+        Key={
+            "Device ID": {
+                "S": node_info['Device ID']
+            }
+        }
+    )    
+    try:
+        print("Checking if Id registered in DB",response['Item'])
+        if response['Item'] == {}:
+            return {"status": "failed", "reason": "id not present in ID database"}
+    except Exception as e:
+        return {"status": "failed", "reason": "id not present in ID database"}
+    data = {}
+    date = datetime.now()
+    tz = pytz.timezone('Asia/Kolkata')
+    current_time = str(date.astimezone(tz))    
+    data['date'] = current_time.split()[0]
+    data['time'] = current_time.split()[1].rstrip('+5:30')
+
+    try:
+        sensor_values_creation = db_handler.create_table_in_database(node_info['Device ID'])
+        sensor_health_creation = db_handler.create_table_in_database(node_info['Device ID']+"_logs")
+    except Exception as e:
+        return {"status":'failed','reason':'node already registered; tables exist'}    
+    
+    row = {"Device ID": {"S":node_info["Device ID"]},
+            "lat": {"S":node_info["lat"]},
+            "lng": {"S":node_info["lng"]},
+            "registration":{"S":"complete"},
+            "time-of-registration":{"S":current_time},
+            "generation-time":{"S":response["Item"]["generation-time"]["S"]}
+            }
+    try:
+        table_insertion_status = db_handler.client.put_item(
+            TableName="Nodes_Available",
+            Item=row
+        )
+        print("After updating registration to complete",table_insertion_status)
+    except Exception as e:
+        print("could not insert location of nodes")
         try:
-            node_info = loads(request.data)
-        except Exception as e:
-            return jsonify({"parsing error ": str(e)})
-        # print(node_info)
-        data = dict()
-        date = datetime.now()
-        tz = pytz.timezone('Asia/Kolkata')
-        current_time = str(date.astimezone(tz))
-        current_time = str(date.astimezone(tz))
-        data['date'] = current_time.split()[0]
-        data['time'] = current_time.split()[1].rstrip('+5:30')
-        if node_info["Device ID"] == "":
-            device_id = str(uuid.uuid4())
+            response = db_handler.client.delete_table(
+                TableName=node_info['Device ID']+"_logs"
+                )
+            response = db_handler.client.delete_table(
+                TableName=node_info['Device ID']
+                )
+        except Exception:
+            pass
+        return {"status": "failed", 'reason': 'error in updating registration'}
+    return {"status":"success","reason":"Node successfully registered"}
+    # add table to existing pool of device
 
-            # creating table for the node
-            status = db_handler.create_table_in_database(device_id)
-            if status == "failed":
-                data['error'] = 'dynamo failed'
-                return jsonify(data)
 
-                # add table to existing pool of device
-            try:
-                row = {"Device ID": device_id, "lat": "", "lng": ""}
-                with nodes_table.batch_writer() as writer:
-                    writer.put_item(Item=row)
-            except Exception as e:
-                print("could not insert into existing nodes")
-                data['error'] = "could not update existing nodes table"
-                return jsonify(data)
+@app.route("/initialise",methods=["GET"])
+def initialise():
+    try:
+        node_info = loads(request.data)
+    except Exception as e:
+        # add logs here
+        return jsonify({"parsing error ": str(e)})
+    # print(node_info)
+    if node_info["Device ID"] == "":
+        return {'status': "failed", "reason": 'empty device id'}
 
-            data['id'] = device_id
-            return jsonify(data)
-        else:
-            try:
-                row = {"Device ID": node_info["Device ID"],
-                       "lat": node_info["lat"], "lng": node_info["lng"]}
-                with nodes_table.batch_writer() as writer:
-                    writer.put_item(Item=row)
-            except Exception as e:
-                print("could not insert location of nodes")
-                return jsonify(data)
-            return jsonify(data)
-
-    else:
-        return {"Status": "Failed", "Reason": "Wrong method, POST method only"}
-
+    response = db_handler.client.get_item(
+        TableName='Nodes_Available',
+        Key={
+            "Device ID": {
+                "S": node_info['Device ID']
+            }
+        }
+    )
+    
+    try:
+        if response['Item']['registration']["S"] != "complete":
+            return {"status": "failed", "reason": "id not registered in ID database"}
+    except Exception:
+        return {"status": "failed", "reason": "id not present in ID database"}
+    data = {}
+    date = datetime.now()
+    tz = pytz.timezone('Asia/Kolkata')
+    current_time = str(date.astimezone(tz))    
+    data['date'] = current_time.split()[0]
+    data['time'] = current_time.split()[1].rstrip('+5:30')
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
