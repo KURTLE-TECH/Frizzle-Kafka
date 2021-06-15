f33c49b6565904baa3d37bf35e691af2e87c9096
Rishith Bhowmick <brishi00@gmail.com>
2021-06-15 10:28:15 +0530

    Added logging to mqtt receiver

--- node_data_receiver/mqtt_receiver.py
+++ node_data_receiver/mqtt_receiver.py
@@ -4,26 +4,29 @@ from datetime import datetime
 from json import loads, dumps
 from confluent_kafka import Producer
 import socket
+import logging
 
+logging.basicConfig(filename='mqtt_receiver.log', level=logging.INFO)
 conf = {'bootstrap.servers': "13.126.242.56:9092",'client.id':'13.126.242.56'}
 producer = Producer(conf)
 producer.flush()
 mqttServer = "13.126.242.56"
 topics = ['node_data']
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code" + str(rc))
+    logging.info(" Connected at: "+datetime.now().strftime("%Y-%m-%d_%H:%M:%S")+" with result code "+str(rc))
     client.subscribe("Frizzle/Sensor_Data")
 
 
 def on_message(client, userdata, msg):
 	node_sensor_values = eval(msg.payload.decode('utf-8'))
+	logging.info(" Received at: "+datetime.now().strftime("%Y-%m-%d_%H:%M:%S")+" Topic: "+str(msg.topic)+" Device ID: "+node_sensor_values["Device ID"])
 	if 'time-stamp' not in node_sensor_values.keys():
 		node_sensor_values['time-stamp'] = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
 	producer.produce(topics[0],key=node_sensor_values['Device ID'], value=dumps(node_sensor_values))
-	print("sent to ",topics[0])
+	logging.info("Sent to "+topics[0]+" at "+datetime.now().strftime("%Y-%m-%d_%H:%M:%S"))
 
 
-client = mqtt.Client("rishi_bhowmi_receiver")
+client = mqtt.Client("frizzle_receiver")
 client.on_connect = on_connect
 client.on_message = on_message
 
