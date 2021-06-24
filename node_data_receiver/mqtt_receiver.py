f708a6776e3aa97e177fe92573c49c953b2e69ed
Rishith Bhowmick <brishi00@gmail.com>
2021-06-24 13:49:34 +0530

    added logic for processing logs

--- node_data_receiver/mqtt_receiver.py
+++ node_data_receiver/mqtt_receiver.py
@@ -6,30 +6,38 @@ from confluent_kafka import Producer
 import socket
 import logging
 
-logging.basicConfig(filename='mqtt_receiver.log', level=logging.INFO)
-conf = {'bootstrap.servers': "13.126.242.56:9092",'client.id':'13.126.242.56'}
-producer = Producer(conf)
+logging.basicConfig(filename='mqtt_receiver.log',filemode="w", level=logging.INFO)
+with open("config.json","r") as f:
+	config = loads(f.read())
+
+producer = Producer(config['producer_config'])
 producer.flush()
-mqttServer = "13.126.242.56"
-topics = ['node_data']
+
+
 def on_connect(client, userdata, flags, rc):
-    logging.info(" Connected at: "+datetime.now().strftime("%Y-%m-%d_%H:%M:%S")+" with result code "+str(rc))
-    client.subscribe("Frizzle/Sensor_Data")
+    #logging.info(" Connected at: "+datetime.now().strftime("%Y-%m-%d_%H:%M:%S")+" with result code "+str(rc))
+    client.subscribe(config['mqtt_topics']['data_node'])    
 
 
 def on_message(client, userdata, msg):
-	node_sensor_values = eval(msg.payload.decode('utf-8'))
-	logging.info(" Received at: "+datetime.now().strftime("%Y-%m-%d_%H:%M:%S")+" Topic: "+str(msg.topic)+" Device ID: "+node_sensor_values["Device ID"])
-	if 'time-stamp' not in node_sensor_values.keys():
-		node_sensor_values['time-stamp'] = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-	producer.produce(topics[0],key=node_sensor_values['Device ID'], value=dumps(node_sensor_values))
-	logging.info("Sent to "+topics[0]+" at "+datetime.now().strftime("%Y-%m-%d_%H:%M:%S"))
+		node_values = eval(msg.payload.decode('utf-8'))
+		logging.info("Received at: "+datetime.now().strftime("%Y-%m-%d_%H:%M:%S")+" Topic: "+str(msg.topic)+" Device ID: "+node_values["Device ID"])
+		if 'time-stamp' not in node_values.keys():
+			node_values['time-stamp'] = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")		
 
+		#print(node_values)		
+		producer.produce(config['kafka_topics']['logs_node'],key=node_values['Device ID'], value=dumps(node_values['Logs']))				
+		del node_values["Logs"]
 
+		#print(node_values)
+		producer.produce(config['kafka_topics']['data_node'],key=node_values['Device ID'], value=dumps(node_values))		
+
+		logging.info("Sent to "+config['kafka_topics']['data_node']+" at "+datetime.now().strftime("%Y-%m-%d_%H:%M:%S"))
+
+logging.info("Started listening")
 client = mqtt.Client("frizzle_receiver")
 client.on_connect = on_connect
 client.on_message = on_message
-
 client.username_pw_set("frizzle_test", "FRIZZLE")
-client.connect(mqttServer, 1883, 60)
+client.connect(config['mqtt_server'], 1883, 60)
 client.loop_forever()
